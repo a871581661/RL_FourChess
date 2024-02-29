ed7ea8e53b72d89351a3a84b262a6ebc18f0854b
xxd <871581661@qq.com>
2024-03-01 01:35:07 +0800

    update grid_list

--- environments/board.py
+++ environments/board.py
@@ -1,8 +1,6 @@
 
-
-
-BOARD_LENGTH = 17
-DIRECTIONS={'left':[0,-1],'right':[0,1],'up':[-1,0],'down':[1,0]}
+BOARD_SHAPE = 17
+BOARD_LENGTH = BOARD_SHAPE-1
 
 RAILWAY_LIST = ['RWay','XWay','CWay']
 ROAD_LIST = ['Flag','Norm']
@@ -35,6 +33,9 @@ board_list= [
     ['None', 'None', 'None', 'None', 'None', 'None', 'Norm', 'Flag', 'Norm', 'Flag', 'Norm', 'None', 'None', 'None', 'None', 'None', 'None', ]
 ]
 
+# gen
+Grid_list = [[None for _ in range(BOARD_SHAPE)] for _ in range(BOARD_SHAPE)]
+
 
 #棋盘格类型规则确定:铁路，普通，行营，大本营等
 class NormGrid(object):
@@ -48,8 +49,7 @@ class NormGrid(object):
         # 判断边界,
         # 方向为上下左右
         connections=[[-1,0],[1,0],[0,-1],[0,1]]
-        y = self.pos[0]
-        x = self.pos[1]
+        y,x = self.pos
         if y == 0:
             connections.pop(0)
         if y == BOARD_LENGTH:
@@ -58,9 +58,22 @@ class NormGrid(object):
             connections.pop(2)
         if x == BOARD_LENGTH:
             connections.pop(3)
+
+        for connection in connections:
+            c_y,c_x = connection
+            if board_list[y+c_y][x+c_x] == 'None':
+                connections.remove(connection)
+
         return connections
 
     def compare(self):
+        pass
+
+class FlagGrid(NormGrid):
+    def __init__(self,pos:list,):
+        super().__init__(pos)
+        self.connections = self._get_connections()
+
 
 
 
@@ -92,7 +105,7 @@ class RWayGrid(NormGrid):
         directions = []
         dirs = [[0, -1], [0, 1], [-1, 0], [1, 0]]
         for dir in dirs:
-            if self.pos[0]+dir[0]>=BOARD_LENGTH or self.pos[1]+dir[1]>=BOARD_LENGTH:
+            if self.pos[0]+dir[0]>=BOARD_LENGTH or self.pos[1]+dir[1]>BOARD_LENGTH:
                 continue
             if board_list[self.pos[0]+dir[0]][self.pos[1]+dir[1]] in RAILWAY_LIST:
                 directions.append(dir)
@@ -124,7 +137,7 @@ class XWayGrid(NormGrid):
         super().__init__(pos)
         self.directions = self._get_directions()
         self.connections = self._get_connections()
-        self.extra_direction = self._get_extra_direction()
+        self.target_direction,self.extra_direction = self._get_extra_direction()
         self.step = [1, 1, 1, 1]
 
     def _get_directions(self):
@@ -132,7 +145,7 @@ class XWayGrid(NormGrid):
         dirs = [[0, -1], [0, 1], [-1, 0], [1, 0]]
         xway_towards= [[1, 1], [1, -1], [-1, 1], [-1,-1]]
         for dir in dirs:
-            if self.pos[0] + dir[0] >= BOARD_LENGTH or self.pos[1] + dir[1] >= BOARD_LENGTH:
+            if self.pos[0] + dir[0] >= BOARD_LENGTH or self.pos[1] + dir[1] > BOARD_LENGTH:
                 continue
             if board_list[self.pos[0] + dir[0]][self.pos[1] + dir[1]] in RAILWAY_LIST:
                 directions.append(dir)
@@ -143,7 +156,7 @@ class XWayGrid(NormGrid):
         directions = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
         for dir in directions:
             dir_y ,dir_x = dir
-            if board_list[y+dir_y][board_list[x+dir_x]] == 'XWay':
+            if board_list[y+dir_y][x+dir_x] == 'XWay':
                 xway_direction = dir
                 break
         xway_y,xway_x = xway_direction
@@ -155,107 +168,22 @@ class XWayGrid(NormGrid):
 
 
 
-
-
-
-
-for i in dir:
-    while board_list.chess ==  empty :
-        list.append(pos)
-
-        if board_list == xway:
-
-
-
 class CWayGrid(NormGrid):
     def __init__(self, pos: list, ):
         super().__init__(pos)
-        self.directions = self._get_directions()
-        self.step = [1, 1, 1, 1]
 
-    def _get_directions(self):
-        directions = []
+        self.directions = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+        self.connections = None
+        self.step = self._get_step()
+
+
+    def _get_step(self):
         dirs = [[0, -1], [0, 1], [-1, 0], [1, 0]]
-        for dir in dirs:
-            if self.pos[0] + dir[0] >= BOARD_LENGTH or self.pos[1] + dir[1] >= BOARD_LENGTH:
-                continue
-            if board_list[self.pos[0] + dir[0]][self.pos[1] + dir[1]] in RAILWAY_LIST:
-                directions.append(dir)
-        return directions
-
-
-
-
-class CWayGrid(NormGrid):
-    def __init__(self,pos:list,):
-        super().__init__(pos)
-        self.up = self.upGrid()
-        self.down = self.downGrid()
-        self.left = self.leftGrid()
-        self.right = self.rightGrid()
-        self.type = 'CWay'
-
-    def upGrid(self):
-        if board_list[self.pos[0]-1][self.pos[1]]=='None':
-            return [self.pos[0]-2,self.pos[1]]
-        else:
-            return [self.pos[0]-1,self.pos[1]]
-    def downGrid(self):
-        if board_list[self.pos[0]+1][self.pos[1]]=='None':
-            return [self.pos[0]+2,self.pos[1]]
-        else:
-            return [self.pos[0]+1,self.pos[1]]
-    def leftGrid(self):
-        if board_list[self.pos[0]][self.pos[1]-1]=='None':
-            return [self.pos[0],self.pos[1]-2]
-        else:
-            return [self.pos[0],self.pos[1]-1]
-    def rightGrid(self):
-        if board_list[self.pos[0]][self.pos[1]+1]=='None':
-            return [self.pos[0],self.pos[1]+2]
-        else:
-            return [self.pos[0],self.pos[1]+1]
-
-
-
-
-
-
-
-
-
-
-
-class XWayGrid(NormGrid):
-    diretions = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
-    def __init__(self,pos:list,):
-        super().__init__(pos)
-        self.up = self.upGrid()
-        self.down = self.downGrid()
-        self.left = self.leftGrid()
-        self.right = self.rightGrid()
-        self.type = 'XWay'
-        self.connection = self._getConnection()
-
-
-    def _getConnection(self):
-
-        for diretion in diretions:
-            if board_list[self.pos[0]+diretion[0]][self.pos[1]+diretion[1]]!='XWay':
-                return [self.pos[0]+diretion[0],self.pos[1]+diretion[1]]
-
-
-
-
-
-def get_railway_move():
-    board_list[x,y].direction
-    for direction in board_list[x,y].directions:
-        while board_list[x,y]
-
-
-
-
+        step = [1,1,1,1]
+        for idx,dir in enumerate(dirs):
+            if board_list[self.pos[0] + dir[0]][self.pos[1] + dir[1]] == 'None':
+                step[idx] = 2
+        return step
 
 
 class CHESS(object):
@@ -269,11 +197,34 @@ class CHESS(object):
 
 
 
-
-
-
-
 def move(move):
     if board_list[move].chess:
         board_list[move].chess.attack()
 
+def creat_Grid_list():
+    for idy,grids in enumerate(board_list):
+        for idx,grid in enumerate(grids):
+            if grid == 'None':
+                Grid_list[idy][idx] = None
+            elif grid == 'Norm':
+                Grid_list[idy][idx] = NormGrid([idy,idx])
+            elif grid == 'Flag':
+                Grid_list[idy][idx] = FlagGrid([idy,idx])
+            elif grid == 'Camp':
+                Grid_list[idy][idx] = CampGrid([idy,idx])
+            elif grid == 'RWay':
+                Grid_list[idy][idx] = RWayGrid([idy,idx])
+            elif grid == 'XWay':
+                Grid_list[idy][idx] = XWayGrid([idy,idx])
+            elif grid == 'CWay':
+                Grid_list[idy][idx] = CWayGrid([idy,idx])
+
+creat_Grid_list()
+
+
+def grids_init():
+    pass
+
+
+if __name__ == '__main__':
+    pass
